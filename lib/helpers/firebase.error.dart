723c62fec1ddbca10f211595af09c8e18e7cd51a
Alexandrebmx5 <alexandrebmx5@gmail.com>
2021-01-07 14:41:44 -0400

    commit finalizado snacknar de aviso de login - 07/01/2021

--- lib/helpers/firebase.error.dart
+++ lib/helpers/firebase.error.dart
@@ -1,23 +1,15 @@
 String getErrorString(String code){
   switch (code) {
-    case 'weak_password':
-      return 'Sua senha é muito fraca.';
     case 'user-not-found':
       return 'Seu e-mail é inválido.';
-    case 'email_already_in_user':
+    case 'email-already-in-use':
       return 'E-mail já está sendo utilizado em outra conta.';
-    case 'invalid_credential':
-      return 'Seu e-mail é inválido.';
     case 'wrong-password':
       return 'Sua senha está incorreta.';
-    case 'user_not_found':
-      return 'Não há usuário com este e-mail.';
     case 'user_disabled':
       return 'Este usuário foi desabilitado.';
     case 'network-request-failed':
       return 'Muitas solicitações. Tente novamente mais tarde.';
-    case 'operation_not_allowed':
-      return 'Operação não permitida.';
 
     default:
       return 'Um erro indefinido ocorreu.';
