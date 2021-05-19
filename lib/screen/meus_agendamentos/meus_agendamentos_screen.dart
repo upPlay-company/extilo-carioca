261bab4e910e37e4151d0194ccc9704a71e075a1
Alexandrebmx5 <alexandrebmx5@gmail.com>
2021-05-19 16:52:28 -0400

    commit - correção tela meus agendamentos - alexandre

--- lib/screen/meus_agendamentos/meus_agendamentos_screen.dart
+++ lib/screen/meus_agendamentos/meus_agendamentos_screen.dart
@@ -1,7 +1,6 @@
 import 'package:extilo_carioca/common/drawer/custom_drawer.dart';
 import 'package:extilo_carioca/common/empty_card.dart';
 import 'package:extilo_carioca/model/agendamento/agendamento_manager.dart';
-import 'package:extilo_carioca/screen/servicos/lista_produtos/lista_all_produtos_screen.dart';
 import 'package:extilo_carioca/style/style_screen_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -13,38 +12,6 @@ class MeusAgendamentosScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return styleScreenPattern(
       child: Scaffold(
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.only(bottom: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                color: Colors.transparent,
-                padding: const EdgeInsets.only(left: 35),
-                child: SizedBox(
-                  height: 44,
-                  width: 180,
-                  child: FloatingActionButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (_) => ListAllProduct()));
-                    },
-                    backgroundColor: Theme.of(context).primaryColor,
-                    child: Text(
-                      'Comprar Produtos',
-                      overflow: TextOverflow.ellipsis,
-                      style:
-                      TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
         backgroundColor: Colors.transparent,
         appBar: AppBar(
           title: const Text('Meus Agendamentos', style: TextStyle(color: Colors.black),),
